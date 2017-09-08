a71da6903dc04b6833f8b8dd36b4baf8b305e830
liuhongnian <liuhongnian16@gmail.com>
2017-09-08 10:21:24 +0800

    项目开始

---
+++ YTKNetwork-Swift/src/BaseNetworkRequest.swift
@@ -0,0 +1,13 @@
+//
+//  BaseNetworkRequest.swift
+//  YTKNetwork-Swift
+//
+//  Created by liuhongnian on 9/8/17.
+//  Copyright © 2017 liuhongnian. All rights reserved.
+//
+
+import UIKit
+
+class BaseNetworkRequest: NSObject {
+
+}
